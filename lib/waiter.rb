c36cf584faf08259fe0891ed7d9762ea36c7a8c6
github username <scott.gloyna01@gmail.com>
2021-01-16 13:02:51 -0600

    commit tree 67072e29e2eafba575463670d09f7fd4e818439e

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,26 @@
-class Waiter
 
+require 'pry'
+class Waiter
+    attr_accessor :name, :experiance
+
+    @@all =[]
+    def initialize (name, experiance)
+        @name = name
+        @experiance = experiance
+        @@all << self
+    end 
+
+    def self.all
+        @@all
+    end
+    def new_meal(customer, total, tip)
+        Meal.new(self,customer,total,tip)
+    end
+    def meals
+        Meal.all.select {|meal| meal.waiter == self}
+    end
+
+    def best_tipper
+        meals.max_by {|meal| meal.tip }.customer
+    end
 end
